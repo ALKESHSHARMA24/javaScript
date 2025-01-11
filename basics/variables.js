b51c40cda6d95400be949a5074a79adbd31070b1
ALKESH SHARMA <91245284+ALKESHSHARMA24@users.noreply.github.com>
2025-01-11 19:14:21 +0530

    Delete basics directory

--- basics/variables.js
+++
@@ -1,7 +0,0 @@
-const accountID=202312128   //used keyword for constant values
-let name="alkesh"           //let keyword used now inplace of var as it solve the issue of var which create function and scrop of variables issue
-var surname="sharma"        //use previously but create issue of scoping
-sports="Cricket"            //memory allocate 
-
-
-console.log(accountID)
